39e933a793538fbd2c2f00a6f4ac4d516e0de718
Yusnardo Tendio <yusnardo.te@metrodata.co.id>
2025-06-19 17:22:39 +0200

    update title and utilize get_prompt_text

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from providers.openai_provider import OpenAIProvider
 from providers.google_provider import GoogleProvider
 from fpdf import FPDF
 import tempfile
-
+from helpers import *
 
 # Load your CSS file
 with open("style.css") as f:
@@ -32,42 +32,28 @@ def get_provider(name: str):
 provider = get_provider(ACTIVE_PROVIDER)
 
 def extract_text_from_pdf(file_path):
-    image_parsing_prompt = """
-        Parse all the text and english text from the image.
-        If it is a transcript, then the output should be a json with keys like these 
-        {
-            name: name of the student,
-            gpa: gpa of the student,
-            study_program: study program of the student in english,
-            degree: degree of the student in english,
-            subject: {
-                subject_name: name of the subject in english,
-                credit: credit of the subject,
-                grade: grade of the subject,
-            }
-        }.
-        if it is not a transcript then parse all the text.
-    """
+    image_parsing_prompt = get_prompt_text("prompt_text/transcript_image_parsing.txt")
+    print(image_parsing_prompt)
     doc = pymupdf.open(file_path)
     image_list = []
     full_text = ""
     for page in doc:
         image_list += page.get_images(full=True)
 
-    if len(image_list) <= 0:
-        for page in doc:
-            full_text += page.get_text()
-    else:
-        try:
-            client = genai.Client(api_key=API_KEYS["image_parsing_api_key"])
-            my_file = client.files.upload(file=file_path)
-            response = client.models.generate_content(
-                model="gemini-2.0-flash",
-                contents=[my_file, image_parsing_prompt],
-            )
-            full_text += response.text
-        except (exceptions.ClientError):
-            return "Unable to parse the image because the given API KEY is not active"
+    # if len(image_list) <= 0:
+    #     for page in doc:
+    #         full_text += page.get_text()
+    # else:
+    #     try:
+    #         client = genai.Client(api_key=API_KEYS["image_parsing_api_key"])
+    #         my_file = client.files.upload(file=file_path)
+    #         response = client.models.generate_content(
+    #             model="gemini-2.0-flash",
+    #             contents=[my_file, image_parsing_prompt],
+    #         )
+    #         full_text += response.text
+    #     except (exceptions.ClientError):
+    #         return "Unable to parse the image because the given API KEY is not active"
     return full_text
 
 
@@ -216,7 +202,7 @@ def evaluate_and_return_pdf_and_text(essay, transcript):
 
 
 # Gradio interface
-with gr.Blocks(css=css, theme=gr.themes.Soft()) as demo:
+with gr.Blocks(css=css, theme=gr.themes.Soft(), title="TUM Application Evaluation") as demo:
     gr.Markdown("## 📄 Upload PDFs / Images", elem_classes="section-title")
 
     with gr.Row(equal_height=True):
